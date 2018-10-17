70b3b4c3473f2f7b2f936f6d180818a92b77f819
Jacob Menashi <jbmenashi@gmail.com>
2018-10-17 20:22:16 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do | phrase | # sorting by each phrase
- 	  phrase.split("").collect do | element |
- 	    binding.pry
-      alphabet.index(element)
+  arr.sort_by do |phrase| # sorting by each phrase
+ 	  phrase.split("").collect do |letter| # splits the phrase into individual letters, then creates a new array with collect that goes through each letter...
+      alphabet.index(letter) # ...and finds their index in the new alphabet. Then it sorts them by that index
     end
   end
 end
