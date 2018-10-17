09eef4f73a2e9999ddface648ac0b8d4f3a69554
Jacob Menashi <jbmenashi@gmail.com>
2018-10-17 20:13:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do | phrase |
- 	  phrase.split("").each do | element |
+ 	  phrase.split("").collect do | element |
       alphabet.index(element)
     end
   end
