b86f008949c75455d19f72e1f336283e716e0682
Jacob Menashi <jbmenashi@gmail.com>
2018-10-17 20:15:44 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do | phrase | # sorting by each phrase
- 	  phrase.split("").collect do | element | 
+ 	  phrase.split("").collect do | element |
+ 	    binding.pry
       alphabet.index(element)
     end
   end
